00490f66fb56590ba1e703ccd0d3a6267f80a410
zhangk0533 <907144187@qq.com>
2017-09-22 08:31:22 +0800

    first commit

---
+++ src/com/zk/test/ActionInfo.java
@@ -0,0 +1,21 @@
+package com.zk.test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ActionInfo extends ActionSupport {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+	
+	public String add(){
+		return SUCCESS;
+	}
+
+}
---
+++ src/com/zk/test/UserAction.java
@@ -0,0 +1,31 @@
+package com.zk.test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	public String add(){
+		System.out.println("name:"+name);
+		System.out.println("age:"+age);
+		return SUCCESS;
+	}
+
+}
